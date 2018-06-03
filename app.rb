fb64f3fdcd584765a73c8d0dfdf7e5f4629869d6
kenzan8000 <kenzan8000@gmail.com>
2018-06-02 23:50:20 -0700

    fix a bug when number of items are more than 25

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 csv_name = "#{ARGV[0]}"
 return unless csv_name.end_with? ".csv"
 name = csv_name.sub(".csv", "")
-json_name = csv_name.sub(".csv", ".json")
 
 require 'json'
 require 'csv'
 
+MAX_ITEMS_PER_JSON = 25
 ITEM_LINE = 0; TYPE_LINE = 1
 
 # read csv and make json
-csv_datas = []
+all_csv_datas = []
 items = []; types = []
 csv_reader = CSV.open(csv_name, 'r', :col_sep => ',')
 csv_reader.each_with_index do |line, index|
@@ -18,10 +18,17 @@ csv_reader.each_with_index do |line, index|
 
   csv_data = { "PutRequest" => { "Item" => { } } }
   items.each_with_index { |item, index| csv_data["PutRequest"]["Item"][item] = { types[index] => line[index] } }
-  csv_datas.push csv_data
+  all_csv_datas.push csv_data
 end
 
 # write json
-File.open(json_name, "w") do |file|
-  file.write({name => csv_datas}.to_json)
+number_of_files = (all_csv_datas.count % MAX_ITEMS_PER_JSON == 0) ? (all_csv_datas.count / MAX_ITEMS_PER_JSON) : (all_csv_datas.count / MAX_ITEMS_PER_JSON) + 1
+for i in 0...number_of_files
+  last_index = (i+1)*MAX_ITEMS_PER_JSON
+  last_index = all_csv_datas.count - 1 if last_index >= all_csv_datas.count
+  csv_datas = all_csv_datas[i*MAX_ITEMS_PER_JSON, last_index]
+  json_name = csv_name.sub(".csv", "_#{i}.json")
+  File.open(json_name, "w") do |file|
+    file.write({name => csv_datas}.to_json)
+  end
 end
